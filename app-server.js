d5796d83499ac1bf46716a7452162ae0b830e9cf
jpbarto <jason.p.barto@gmail.com>
2021-12-14 01:28:48 +0000

    adding modified, working js server script

--- app-server.js
+++ app-server.js
@@ -1,13 +1,31 @@
 var http = require("http");
+var mysql = require('mysql');
+var AWS = require('aws-sdk');
+
+var db = mysql.createConnection({
+  host: 'hm1gsu8jb9nqdvg.ckqc4dnfzj72.eu-west-2.rds.amazonaws.com',
+  user: 'admin',
+  password: 'DbPassword',
+  database: 'iptracker'
+});
+
+db.connect();
 
 var SERVER_PORT = 8080;
-var WEB_SITE_IMAGE_URL =
-  "https://i.chzbgr.com/full/875511040/h8EB4D6E9/famous-cat-meme-which-started-and-launched-the-website-i-can-haz-cheezburger";
 
-function writeResponse(message, content, link, res) {
+var metadata = {};
+var META = new AWS.MetadataService();
+META.request('/latest/dynamic/instance-identity/document', function (err, data) {
+  metadata = JSON.parse(data);
+  META.request('/latest/meta-data/local-hostname', function (err, data) {
+    metadata['privateHostname'] = data;
+  });
+});
+
+function writeResponse(message, content, link, imageUrl, res) {
   // set response content
   res.write(`
-	    <!DOCTYPE html>
+            <!DOCTYPE html>
             <html>
                 <head>
                     <meta charset="utf-8">
@@ -18,7 +36,7 @@ function writeResponse(message, content, link, res) {
                     <p>${message}</p>
                     <p>${content}</p>
                     <p><a href="${link}">click here to go to other page</a></p>
-                    <img src="${WEB_SITE_IMAGE_URL}" alt="" width="700">
+                    <img src="${imageUrl}" alt="" width="700">
                 </body>
             </html>`);
 }
@@ -26,39 +44,55 @@ function writeResponse(message, content, link, res) {
 var server = http.createServer(function (req, res) {
   console.log("Node.js web server handling request..");
   if (req.url == "/") {
+    var webSiteImageUrl = "https://aws-well-architected-labs-ohio.s3.us-east-2.amazonaws.com/images/Cirque_of_the_Towers.jpg";
     message = `Data from the metadata API`;
     content = `
-      accountId<br/>
-      amiId<br/>
-      availabilityZone<br/>
-      instanceId<br/>
-      instanceType<br/>
-      privateHostname<br/>
-      privateIp<br/>
+      accountId: ${metadata.accountId}<br/>
+      amiId: ${metadata.imageId}<br/>
+      availabilityZone: ${metadata.availabilityZone}<br/>
+      instanceId: ${metadata.instanceId}<br/>
+      instanceType: ${metadata.instanceType}<br/>
+      privateHostname: ${metadata.privateHostname}<br/>
+      privateIp: ${metadata.privateIp}<br/>
     `;
+
+    db.query("INSERT INTO hits(ip) values (?)", [req.socket.remoteAddress], function (error, results, fields) {
+      if (error) throw error;
+    });
 
     // set response header
     res.writeHead(200, { "Content-Type": "text/html" });
 
-    writeResponse(message, content, "/data", res);
+    writeResponse(message, content, "/data", webSiteImageUrl, res);
 
     res.end();
-  }
-  if (req.url == "/data") {
-    console.log("Providing detailed response");
-    message = `Data from the database`;
-    content = `
-      ip = 127.0.0.1   time = 10:27 am<br/>
-      ip = 10.0.0.1   time = 09:23 am<br/>
-    `;
+  } else
+    if (req.url == "/data") {
+      console.log("Providing detailed response");
 
-    // set response header
-    res.writeHead(200, { "Content-Type": "text/html" });
-    writeResponse(message, content, "/", res);
-    res.end();
-  } else {
-    res.end("Invalid Request!");
-  }
+
+      // set response header
+      res.writeHead(200, { "Content-Type": "text/html" });
+
+      message = `Data from the database`;
+
+      db.query('SELECT * FROM hits ORDER BY time DESC LIMIT 10', function (error, results, fields) {
+        if (error) throw error;
+        // console.log (results);
+        content = '';
+        Object.keys(results).forEach(function (key) {
+          var row = results[key];
+          // console.log (row);
+          content += 'ip = ' + row.ip + ' \t time = ' + row.time + '<br/>';
+        });
+
+              writeResponse(message, content, "/", "", res);
+              res.end();
+      });
+
+    } else {
+      res.end("Invalid Request!");
+    }
 });
 
 server.listen(SERVER_PORT);
